32f72483b27edf05436110b42c47f8743080ba7b
caiordev <caioreisdev@gmail.com>
2024-11-27 12:59:02 -0300

    feat: reconhecendo numeros inteiros

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -1,11 +1,5 @@
 package br.ufma.ecp;
 
-import static br.ufma.ecp.token.TokenType.*;
-
-
-
-import br.ufma.ecp.token.Token; 
-
 public class App 
 {
 
@@ -14,11 +8,9 @@ public class App
     {
 
     
-        String input = "45  + if - \"ola\" laranja 876";
+        String input = "289";
         Scanner scan = new Scanner (input.getBytes());
-        for (Token tk = scan.nextToken(); tk.type != EOF; tk = scan.nextToken()) {
-            System.out.println(tk);
-        }
+        System.out.println(scan.nextToken());
 
         /*
         Parser p = new Parser (input.getBytes());
