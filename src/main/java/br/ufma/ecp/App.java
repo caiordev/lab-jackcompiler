b03ae0cf820eeefc652daff6031ff33898f2c86b
caiordev <caioreisdev@gmail.com>
2024-11-28 18:25:50 -0300

    feat: ignorando comentários

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -11,7 +11,15 @@ public class App
     {
 
 
-        String input = "45 \"hello\" variavel + while < , if";
+        String input = """
+      // é um comentario 10
+      45 \"hello\" variavel + while < , if
+      /*
+      comentario em bloco
+      */
+      42 ola
+      
+      """;
         Scanner scan = new Scanner (input.getBytes());
         for (Token tk = scan.nextToken(); tk.type != TokenType.EOF; tk = scan.nextToken()) {
             System.out.println(tk);
--- src/main/java/br/ufma/ecp/Scanner.java
+++ src/main/java/br/ufma/ecp/Scanner.java
@@ -62,6 +62,31 @@ public class Scanner {
         }
     }
 
+    private void skipBlockComments() {
+        boolean endComment = false;
+        advance();
+        while (!endComment) {
+            advance();
+            char ch = peek();
+            if ( ch == 0) { // eof, lexical error
+                System.exit(1);
+            }
+
+            if (ch == '*') {
+                for (ch = peek(); ch == '*';  advance(), ch = peek()) ;
+                if (ch == '/') {
+                    endComment = true;
+                    advance();
+                }
+            }
+
+        }
+    }
+
+    private void skipLineComments() {
+        for (char ch = peek(); ch != '\n' && ch != 0;  advance(), ch = peek()) ;
+    }
+
 
     public Token nextToken () {
 
@@ -82,8 +107,17 @@ public class Scanner {
             case '"':
                 return string();
             case '/':
-                advance();
-                return new Token (TokenType.SLASH,"/");
+                if (peekNext() == '/') {
+                    skipLineComments();
+                    return nextToken();
+                } else if (peekNext() == '*') {
+                    skipBlockComments();
+                    return nextToken();
+                }
+                else {
+                    advance();
+                    return new Token (TokenType.SLASH,"/");
+                }
             case '*':
                 advance();
                 return new Token (TokenType.ASTERISK,"*");
@@ -198,6 +232,15 @@ public class Scanner {
         if (current < input.length)
             return (char)input[current];
         return 0;
+    }
+
+    private char peekNext () {
+        int next = current + 1;
+        if ( next  < input.length) {
+            return (char)input[next];
+        } else {
+            return 0;
+        }
     }
 
 
